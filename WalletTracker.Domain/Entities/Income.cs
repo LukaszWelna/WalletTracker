0884b76f94a4f5e8197d22aac76d78fab4bda51d
Lukasz Welna <lukasz.welna96@gmail.com>
2024-04-14 19:55:42 +0200

    Added custom Name property to Identity

--- WalletTracker.Domain/Entities/Income.cs
+++ WalletTracker.Domain/Entities/Income.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WalletTracker.Domain.Models;
 
 namespace WalletTracker.Domain.Entities
 {
@@ -11,7 +13,7 @@ namespace WalletTracker.Domain.Entities
     {
         public int Id { get; set; }
         public string UserId { get; set; } = default!;
-        public IdentityUser User { get; set; } = default!;
+        public ApplicationUser User { get; set; } = default!;
         public int CategoryId { get; set; }
         public IncomeCategoryAssignedToUser Category { get; set; } = default!;
         public decimal Amount { get; set; }
---
+++ WalletTracker.Domain/Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletTracker.Domain.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(100)]
+        [StringLength(100)]
+        [Display(Name = "Name")]
+        public string? Name { get; set; }
+    }
+}
